33f63e3d6bf0c0c102742cc5d9440077414588c8
songawee <dennis@songawee.com>
2013-07-28 10:34:57 -0600

    Added the setType function in the constructor

--- monkey.cpp
+++ monkey.cpp
@@ -2,8 +2,14 @@
 
 #include <iostream>
 #include "monkey.h"
+#include <string>
 using namespace std;
 
+Monkey::Monkey(string animalName) {
+  Animal::setName(animalName);
+  Animal::setType("Monkey");
+}
+
 void Monkey::talk() const {
-  cout << "Eep! Eep!" << endl;
+  cout << "Eep! Eep!";
 }
