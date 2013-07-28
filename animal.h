606e5c59da3247a6ae5744a650a1cb3bbcacd54e
songawee <dennis@songawee.com>
2013-07-28 10:33:15 -0600

    Added the type variable and pubic get/set functions for accessing the data

--- animal.h
+++ animal.h
@@ -1,13 +1,20 @@
 // animal.h
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
+#include <string>
 using namespace std;
 
 class Animal 
 {
   public: 
     virtual void talk() const = 0;
+    void setName(string);
+    string getName();
+    void setType(string);
+    string getType();
+  private:
+    string name; 
+    string type;
 };
 
 #endif
